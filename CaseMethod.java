ec2fea3f00914437e5b6c7e0827fb2da587ab7cc
Aura Bintang Aprilian <auraaprilian0@gmail.com>
2025-12-10 10:27:58 +0700

    Step 2

--- CaseMethod.java
+++ CaseMethod.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class CaseMethod {
     static Scanner input = new Scanner(System.in);
+    static int daftar = 0, n = 100;
+    static String[][] data = new String[n][5];
 
     static void menu(){
         System.out.println(
@@ -14,6 +16,45 @@ public class CaseMethod {
             );
             System.out.print("Pilih menu (1-5): ");
     }
+    static void menu1(){
+        if (daftar >= n) {
+            System.out.println("Pendaftar sudah penuh.");
+            return;
+        }
+
+        System.out.print("Nama Mahasiswa\t\t\t\t: ");
+        data[daftar][0] = input.nextLine();
+        System.out.print("NIM\t\t\t\t\t: ");
+        data[daftar][1] = input.nextLine();
+        System.out.print("IPK terakhir\t\t\t\t: ");
+        data[daftar][2] = input.nextLine();
+
+        String jenis;
+        while (true) {
+            System.out.print("Jenis Beasiswa (Reguler/Unggulan/Riset)\t: ");
+            jenis = input.nextLine();
+            if (jenis.equalsIgnoreCase("Reguler") || jenis.equalsIgnoreCase("Unggulan") || jenis.equalsIgnoreCase("Riset")){
+                break;
+            }
+            System.out.println("Jenis Beasiswa tidak valid.");
+            return;
+        }
+        data[daftar][3] = jenis;
+
+        System.out.print("Penghasilan Orang Tua (maks 2000000)\t: ");
+        int gaji = input.nextInt();
+        input.nextLine();
+
+        if (gaji > 2000000) {
+            System.out.println("Pendaftaran dibatalkan karena penghasilan melebihi batas maksimal.");
+            return;
+        }
+        data[daftar][4] = Integer.toString(gaji);
+        
+        daftar++;
+        System.out.println("Pendaftar berhasil disimpan. Total pendaftar: " + daftar);
+    }
+
 
     
 }
